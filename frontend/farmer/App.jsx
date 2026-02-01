88120881d4256eb929a52c034a0ac9612ce7278a
Shivani0507 <shivanikumar0705@gmail.com>
2026-02-02 00:44:28 +0530

    Reorganized Farmer frontend folder structure

--- frontend/farmer/App.jsx
+++ frontend/farmer/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import DashboardPage from "./pages/DashboardPage.jsx";
-import CropRegistrationPage from "./pages/CropRegistrationPage.jsx";
-import CropStatusPage from "./pages/CropStatusPage.jsx";
-import FailureReportPage from "./pages/FailureReportPage.jsx";
+import DashboardPage from "./src/pages/DashboardPage.jsx";
+import CropRegistrationPage from "./src/pages/CropRegistrationPage.jsx";
+import CropStatusPage from "./src/pages/CropStatusPage.jsx";
+import FailureReportPage from "./src/pages/FailureReportPage.jsx";
 
 function App() {
   return (
