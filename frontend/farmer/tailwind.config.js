537508a1e9755f77d26743ded8d93984466fd50e
Shivani0507 <shivanikumar0705@gmail.com>
2026-02-02 20:57:11 +0530

    Reorganized Farmer frontend folder structure

--- frontend/farmer/tailwind.config.js
+++ frontend/farmer/tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.jsx",
-    "./App.jsx",
-    "./src/pages/**/*.{js,jsx}",
-    "./src/components/**/*.{js,jsx}"
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
